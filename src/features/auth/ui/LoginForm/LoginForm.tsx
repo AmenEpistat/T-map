71dfcdf4a80c66c1da9056f1941abc553b945e7e
tenine <tenine1337@gmail.com>
2026-04-19 18:06:02 +0300

    feat(auth): render LoginForm markup with AntD fields

--- src/features/auth/ui/LoginForm/LoginForm.tsx
+++ src/features/auth/ui/LoginForm/LoginForm.tsx
@@ -1,3 +1,57 @@
+import { Form, Input, Button } from 'antd';
+import { Link } from 'react-router-dom';
+import styles from './LoginForm.module.scss';
+
+interface LoginFormValues {
+    email: string;
+    password: string;
+}
+
 export const LoginForm = () => {
-    return <div>LoginForm placeholder</div>;
+    const [form] = Form.useForm<LoginFormValues>();
+
+    const handleFinish = (values: LoginFormValues) => {
+        // eslint-disable-next-line no-console
+        console.log('login submit:', values);
+    };
+
+    return (
+        <Form
+            form={form}
+            layout='vertical'
+            onFinish={handleFinish}
+            className={styles.form}
+            requiredMark={false}
+        >
+            <div className={styles.fields}>
+                <Form.Item name='email' noStyle>
+                    <Input
+                        placeholder='Email'
+                        size='large'
+                        autoComplete='email'
+                    />
+                </Form.Item>
+
+                <Form.Item name='password' noStyle>
+                    <Input.Password
+                        placeholder='Пароль'
+                        size='large'
+                        autoComplete='current-password'
+                    />
+                </Form.Item>
+            </div>
+
+            <Form.Item noStyle>
+                <Button type='primary' htmlType='submit' size='large' block>
+                    Войти
+                </Button>
+            </Form.Item>
+
+            <div className={styles.footer}>
+                <Link to='/auth/register' className={styles.link}>
+                    Ещё не зарегистрированы?
+                </Link>
+            </div>
+        </Form>
+    );
 };
