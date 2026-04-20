120f8c1c0c31b54d31588b28d03cedf8654283d1
luiza <luizaxakieva@gmail.com>
2026-04-20 13:56:43 +0300

    feat(map): add MapSearch

---
+++ src/features/map/components/MapSearch/MapSearch.tsx
@@ -0,0 +1,38 @@
+import { Button, Input } from 'antd';
+import { MenuOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+import styles from './MapSearch.module.scss';
+import SearchPanel from '@/features/map/components/SearchPanel/SearchPanel';
+
+const MapSearch = () => {
+    const [isSearchOpen, setSearchOpen] = useState(false);
+
+    return (
+        <>
+            <div className={styles['map-search']}>
+                <Button
+                    className={styles['map-search__button']}
+                    onClick={() =>
+                        console.log('я пока думаю что делать с профилем')
+                    }
+                >
+                    <MenuOutlined />
+                </Button>
+                <Input
+                    className={styles['map-search__input']}
+                    placeholder='Поиск и выбор мест'
+                    readOnly
+                    onFocus={() => setSearchOpen(true)}
+                    onClick={() => setSearchOpen(true)}
+                />
+            </div>
+
+            <SearchPanel
+                isOpen={isSearchOpen}
+                onClose={() => setSearchOpen(false)}
+            />
+        </>
+    );
+};
+
+export default MapSearch;
--- src/pages/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage.tsx
@@ -1,9 +1,11 @@
 import { MapView } from '@/features/map';
 import ZoomControls from '@/features/map/components/ZoomControls/ZoomControls.tsx';
+import MapSearch from '@/features/map/components/MapSearch/MapSearch.tsx';
 
 export const MapPage = () => {
     return (
         <section>
+            <MapSearch />
             <MapView />
             <ZoomControls />
         </section>
