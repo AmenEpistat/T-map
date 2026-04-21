4e49803d70cc17ca1cda8369015349f4194fd155
luiza <luizaxakieva@gmail.com>
2026-04-21 10:32:22 +0300

    style: add icons

---
+++ src/features/map/components/LayerFilter/components/ui/index.ts
@@ -0,0 +1,4 @@
+export { LayerOutlined } from './LayerOutlined/LayerOutlined.tsx';
+export { FoodOutlined } from '@/features/map/components/LayerFilter/components/ui/FoodFilled/FoodFilled.tsx';
+export { EntertainmentFilled } from '@/features/map/components/LayerFilter/components/ui/EntertainmentFilled/EntertainmentFilled.tsx';
+export { ShoppingFilled } from '@/features/map/components/LayerFilter/components/ui/ShoppingFilled/ShoppingFilled.tsx';
