600d74384605bcd4e4b4ddcb67be997e36da8e4a
tenine <tenine1337@gmail.com>
2026-04-22 13:44:27 +0300

    refactor(pages): move ProfilePage into profile/ slice

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import React from 'react';
 import { AuthPage } from '@/pages/auth';
 import { MapPage } from '@/pages/map';
-import { ProfilePage } from '@/pages/ProfilePage';
+import { ProfilePage } from '@/pages/profile';
 import { ProtectedRoute } from '@/shared/ui';
 
 export const router = createBrowserRouter([
