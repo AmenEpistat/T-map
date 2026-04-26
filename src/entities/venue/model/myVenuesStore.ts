94d1a628ba7e2f342532b5b5a77cacf6c60d7693
tenine <tenine1337@gmail.com>
2026-04-26 16:25:17 +0300

    feat(entities/venue): implement load actions in my venues store

--- src/entities/venue/model/myVenuesStore.ts
+++ src/entities/venue/model/myVenuesStore.ts
@@ -1,6 +1,10 @@
 import { makeAutoObservable } from 'mobx';
 import { RequestState } from '@/shared/api/RequestState';
+import { businessVenuesApi } from '@/entities/venue';
 import type { OwnerVenue } from './types';
+
+const wrap = <T>(promise: Promise<T>): Promise<{ data: T }> =>
+    promise.then((data) => ({ data }));
 
 class MyVenuesStore {
     list = new RequestState<OwnerVenue[]>();
@@ -9,6 +13,14 @@ class MyVenuesStore {
     constructor() {
         makeAutoObservable(this);
     }
+
+    loadAll = async (): Promise<void> => {
+        await this.list.execute(wrap(businessVenuesApi.getMyVenues()));
+    };
+
+    loadById = async (id: string): Promise<void> => {
+        await this.current.execute(wrap(businessVenuesApi.getMyVenueById(id)));
+    };
 
     clear = (): void => {
         this.list = new RequestState<OwnerVenue[]>();
