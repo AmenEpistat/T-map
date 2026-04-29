c71642045bc328545f05df6cb291bf5f89481f61
luiza <luizaxakieva@gmail.com>
2026-04-29 09:56:28 +0300

    style: edit desktop styles for popup

---
+++ src/shared/ui/Icons/SharedFilled/SharedFilled.tsx
@@ -0,0 +1,20 @@
+const SharedFilled = () => {
+    return (
+        <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            fill='none'
+            viewBox='0 0 24 24'
+        >
+            <path
+                fill='#ccc'
+                fillRule='evenodd'
+                d='M12 24c6.627 0 12-5.373 12-12S18.627 0 12 0 0 5.373 0 12s5.373 12 12 12M9.939 9.217c-2.833 1.28-4.774 3.668-5.731 7.048L4 17l.702-.727.007-.008a10.3 10.3 0 0 1 2.435-1.912l-.074.032a12.95 12.95 0 0 1 5.37-1.29h.712v3.351L19 10.724 13.152 5v1.676c0 1.669-.001 1.676-.237 1.676l-.078.01a12.6 12.6 0 0 0-2.898.855'
+                clipRule='evenodd'
+            />
+        </svg>
+    );
+};
+
+export default SharedFilled;
