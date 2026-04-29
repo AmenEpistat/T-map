e744c516769ac0648fdd92f857d1094b2c85e4ca
luiza <luizaxakieva@gmail.com>
2026-04-29 12:01:03 +0300

    refactor: changes from constant to enum

--- src/entities/map/model/constants.ts
+++ src/entities/map/model/constants.ts
@@ -6,11 +6,11 @@ export const MAP_CATEGORIES: MapCategory[] = [
     'shopping',
 ] as const;
 
-export const MAPPING_CATEGORIES: Record<MapCategory, string> = {
-    food: 'Еда',
-    entertainment: 'Развлечения',
-    shopping: 'Шоппинг',
-} as const;
+export enum MAPPING_CATEGORIES {
+    food = 'Еда',
+    entertainment = 'Развлечения',
+    shopping = 'Шоппинг',
+}
 
 export const BOUNDS_KEYS: (keyof Bounds)[] = [
     'swLng',
