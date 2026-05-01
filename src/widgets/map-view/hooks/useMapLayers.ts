7a8bfcd7e0ccfef4880d93ce7dd272660ab06a43
luiza <luizaxakieva@gmail.com>
2026-05-01 21:01:52 +0300

    feat(map): implement IconLayer and TextLayer for POI visualization

--- src/widgets/map-view/hooks/useMapLayers.ts
+++ src/widgets/map-view/hooks/useMapLayers.ts
@@ -3,22 +3,46 @@ import { H3HexagonLayer } from '@deck.gl/geo-layers';
 import {
     getBorderColor,
     getClusterColor,
+    getNameColor,
 } from '@/widgets/map-view/utils/colorUtils.ts';
-
 import type { ClusterDataType } from '@/entities/map/model/types.ts';
 import { mapStore } from '@/entities/map';
+import iconsPng from '@/shared/assets/icons.png';
+import iconsJson from '@/shared/assets/icons.json';
+import { IconLayer, TextLayer } from '@deck.gl/layers';
+import type { PublicVenue } from '@/entities/venue';
 
-export const useMapLayers = (data: ClusterDataType[]) => {
+export const useMapLayers = () => {
+    const clusters = mapStore.clusters.data?.clusters;
+    const venues = mapStore.venues.data || [];
+
     const maxTx = useMemo(() => {
-        if (!data || data.length === 0) return 0;
-        return data.reduce((max, d) => Math.max(max, d.txCount), 0);
-    }, [data]);
+        if (!clusters || clusters.length === 0) return 0;
+        return clusters.reduce((max, d) => Math.max(max, d.txCount), 0);
+    }, [clusters]);
+
+    const iconMapping = iconsJson.frames.reduce(
+        (acc, value) => {
+            const cleanKey = value.filename.replace('.svg', '');
+            acc[cleanKey] = {
+                x: value.frame.x,
+                y: value.frame.y,
+                width: value.frame.w,
+                height: value.frame.h,
+                anchorY: value.frame.h,
+                anchorX: value.frame,
+            };
+
+            return acc;
+        },
+        {} as Record<string, any>
+    );
 
     const layers = useMemo(
         () => [
             new H3HexagonLayer<ClusterDataType>({
                 id: 'h3-hexagon-layer',
-                data,
+                data: clusters,
                 getHexagon: (d) => d.h3Index,
                 elevationScale: 0,
                 stroked: true,
@@ -39,8 +63,29 @@ export const useMapLayers = (data: ClusterDataType[]) => {
                     mapStore.setClusterIndex(object.h3Index);
                 },
             }),
+            new IconLayer<PublicVenue>({
+                id: 'poi-icons',
+                data: venues,
+                iconAtlas: iconsPng,
+                iconMapping: iconMapping,
+                getIcon: (d) => d.category,
+                getSize: 40,
+                getPosition: (d) => [d.lng, d.lat],
+                pickable: true,
+            }),
+            new TextLayer<PublicVenue>({
+                id: 'poi-labels',
+                data: venues,
+                getPosition: (d) => [d.lng, d.lat],
+                getText: (d) => d.name,
+                getSize: 14,
+                getColor: (d: PublicVenue) => getNameColor(d.category),
+                fontFamily: 'Inter, sans-serif',
+                fontWeight: 'bold',
+                getPixelOffset: [10, -50],
+            }),
         ],
-        [data, maxTx]
+        [clusters, venues, maxTx]
     );
 
     return layers;
