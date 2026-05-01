814576e612b15d2a6aa45530e025665c7f813fb0
tenine <tenine1337@gmail.com>
2026-05-01 14:18:13 +0300

    feat(my-venues-list): add empty state component

--- src/pages/business/ui/VenuesPage/VenuesPage.tsx
+++ src/pages/business/ui/VenuesPage/VenuesPage.tsx
@@ -1,5 +1,6 @@
 import { observer } from 'mobx-react-lite';
 import styles from './VenuesPage.module.scss';
+import { EmptyState } from '@/features/my-venues-list/ui/EmptyState/EmptyState';
 
 export const VenuesPage = observer(() => {
     return (
@@ -12,9 +13,7 @@ export const VenuesPage = observer(() => {
             </header>
 
             <section className={styles['venues-page__content']}>
-                <p className={styles['venues-page__placeholder']}>
-                    Список ваших заведений появится здесь.
-                </p>
+                <EmptyState />
             </section>
         </div>
     );
