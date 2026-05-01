3be0666f6b33404b818a784cf7f331fe129ae0ec
tenine <tenine1337@gmail.com>
2026-05-01 14:40:12 +0300

    feat(my-venues-list): compose list with loading and error states

--- src/pages/business/ui/VenuesPage/VenuesPage.tsx
+++ src/pages/business/ui/VenuesPage/VenuesPage.tsx
@@ -1,6 +1,6 @@
 import { observer } from 'mobx-react-lite';
 import styles from './VenuesPage.module.scss';
-import { EmptyState } from '@/features/my-venues-list/ui/EmptyState/EmptyState';
+import { MyVenuesList } from '@/features/my-venues-list/ui/MyVenuesList/MyVenuesList';
 
 export const VenuesPage = observer(() => {
     return (
@@ -13,7 +13,7 @@ export const VenuesPage = observer(() => {
             </header>
 
             <section className={styles['venues-page__content']}>
-                <EmptyState />
+                <MyVenuesList />
             </section>
         </div>
     );
