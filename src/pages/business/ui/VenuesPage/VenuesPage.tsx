b38938a52a1646c24430d4ad8524d70d8cbada1c
tenine <tenine1337@gmail.com>
2026-05-02 16:51:07 +0300

    feat(pages/business): hide inactive column on mobile

--- src/pages/business/ui/VenuesPage/VenuesPage.tsx
+++ src/pages/business/ui/VenuesPage/VenuesPage.tsx
@@ -1,9 +1,13 @@
 import { observer } from 'mobx-react-lite';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { MyVenuesList } from '@/features/my-venues-list';
+import { classNames } from '@/shared/utils/classNames';
 import styles from './VenuesPage.module.scss';
 
 export const VenuesPage = observer(() => {
+    const location = useLocation();
+    const isCreating = location.pathname === '/business/new';
+
     return (
         <div className={styles['venues-page']}>
             <header className={styles['venues-page__header']}>
@@ -14,10 +18,24 @@ export const VenuesPage = observer(() => {
             </header>
 
             <div className={styles['venues-page__columns']}>
-                <div className={styles['venues-page__list']}>
+                <div
+                    className={classNames(
+                        styles['venues-page__list'],
+                        isCreating
+                            ? styles['venues-page__list--hidden-on-mobile']
+                            : ''
+                    )}
+                >
                     <MyVenuesList />
                 </div>
-                <div className={styles['venues-page__form']}>
+                <div
+                    className={classNames(
+                        styles['venues-page__form'],
+                        !isCreating
+                            ? styles['venues-page__form--hidden-on-mobile']
+                            : ''
+                    )}
+                >
                     <Outlet />
                 </div>
             </div>
