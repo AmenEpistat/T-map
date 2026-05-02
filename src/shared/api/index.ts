ad6f9883ee61b6d5f79d5229dd6137f10ab1ff7d
luiza <luizaxakieva@gmail.com>
2026-05-02 21:28:53 +0300

    feat(api): add ErrorMessage Type Guard in RequestState

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -2,3 +2,4 @@ export { apiClient, registerAuthCallbacks, refreshAuthSession } from './client';
 export { getAccessToken, setAccessToken } from './token-holder';
 export * from './types';
 export { RequestState } from './RequestState.ts';
+export type { ErrorMessage, isErrorMessage } from './types/error.ts';
