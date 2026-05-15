dae161c92da4adbbfd57139d3020a864b5ff2c19
tenine <tenine1337@gmail.com>
2026-05-15 16:51:35 +0300

    feat: add admin button in profile

--- src/features/profile-panel/ui/ProfileAuthHeader/ProfileAuthHeader.tsx
+++ src/features/profile-panel/ui/ProfileAuthHeader/ProfileAuthHeader.tsx
@@ -28,6 +28,14 @@ const ProfileAuthHeader = observer(({ onClose, isMobile }: Props) => {
                 </Button>
             ),
         },
+        authStore.isAdmin && {
+            key: '4',
+            label: (
+                <Button onClick={() => navigate('/admin')} type={'text'}>
+                    Админ-панель
+                </Button>
+            ),
+        },
         {
             key: '2',
             label: (
